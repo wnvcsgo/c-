77762c15882c94618da033accf1e8bc36505ae9e
wnvcsgo <wnvcsgo@gmail.com>
2016-08-27 13:37:40 +0900

    Add files via upload

---
+++ add up 1~x ( even num ).cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+	
+ 	
+int main ()
+{
+	int sum = 0;
+		
+	for(int i =1; i<11; i = i+1)
+   {
+       sum = sum+2;
+	   
+	   printf("The sum "
+	   "is : %d, %d, %d, %d \n",i, 3, 6, sum);
+	}
+
+   return 0;
+}   
+
