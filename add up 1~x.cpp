eb9a89077beee3b04bf77d2101753ed93af82cdd
wnvcsgo <wnvcsgo@gmail.com>
2016-08-27 12:44:52 +0900

    Add files via upload

---
+++ add up 1~x.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+	
+ 	
+int main ()
+{
+
+int sum = 0;	
+	for(int i =1; i<1001; i = i+1)
+   {
+       sum = sum+i;
+	   printf("The sum of 1 to 1000 is %d \n",sum);
+	  
+   }
+	
+   return 0;
+}   
+
